f6a0ee166ec0f002d70f89d4856d7a9f52ad86c1
Elijah <esalberg100@icloud.com>
2018-09-10 11:34:07 -0600

    lots of small changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// warning this script is entirely janky
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
 
@@ -17,7 +17,7 @@ function createUser() {
     console.log (errorCode);
      //do error stuff
     });
-    
+
     sendEmailVerification();
 
 }
@@ -42,29 +42,31 @@ function deleteUser () {
       // User deleted.
     }).catch(function(error) {
       // An error happened.
-        alert("Fuck, an error occured: " + error.messsage);
+        alert("Error: " + error.messsage);
     });
 }
 
 function resetPassword () {
-    
+
 var auth = firebase.auth();
 var emailAddress = auth.currentUser.email;
 
 auth.sendPasswordResetEmail(emailAddress).then(function() {
   // Email sent.
-    document.getElementById("passwordHelpBlock").style.display = "block";
+    document.getElementById("passwordLoginHelpBlock").style.display = "block";
 }).catch(function(error) {
   // An error happened.
-    alert("Fuck, an error occured: " + error.messsage);
+    alert("Error: " + error.messsage);
 });
 }
 
 function logOut () {
     firebase.auth().signOut().then(function() {
       // Sign-out successful.
+        window.location = '/';
     }).catch(function(error) {
       // An error happened.
+      alert("Error: " + error.messsage);
     });
 }
 
@@ -79,69 +81,107 @@ function updateProfile () {
       // An error happened.
     });
 }
-    
+
+function logInGoogle () {
+    // Create an instance of the Google provider object
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    // To apply the default browser preference instead of explicitly setting it.
+    firebase.auth().useDeviceLanguage();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+     window.location.reload(true);
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+        alert("Error: " + errorCode + " - " + errorMessage)
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+
+}
+
+function logIn () {
+  firebase.auth().signInWithEmailAndPassword(document.getElementById("inputEmail").value, document.getElementById("inputPassword").value)
+    .catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  if (errorCode === 'auth/wrong-password') {
+    alert('Wrong password.');
+  } else {
+    alert(errorMessage);
+  }
+  console.log(error);
+});
+  $('#loginModal').modal('hide');
+}
+
+getLocation();
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
+}
+function showPosition(position) {
+    console.log("Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude);
+}
+
 window.onload = function() {
-if(window.location.pathname == '/index.html') {
+
+if(window.location.pathname == '/index.html' || window.location.pathname == '/') {
+
+
     var whenInput = document.getElementById('when');
     var whereInput = document.getElementById('autocomplete');
-    var submitButton = document.getElementById('postButton');  
+    var submitButton = document.getElementById('postButton');
     var postsLists = document.getElementById('postsLists');
-    
+
     submitButton.addEventListener('click', handleSubmit);
 
-    const postRef = firebase.firestore().collection("posts").orderBy("createdAt", 'desc').limit(5);
+    const postRef = firebase.firestore().collection("posts").orderBy("createdAt", 'desc').limit(10);
 
-    
     postRef.onSnapshot(function(docSnapShot) {
     postsLists.innerHTML = "";
+
     docSnapShot.forEach(function(doc) {
     post = doc.data();
     post.id = doc.id;
-        
-    var request = {
-      placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4',
-      fields: ['name', 'rating', 'formatted_phone_number', 'geometry']
-    };
-    
-    
+    var cardElement = document.createElement("div");
+    cardElement.classList.add('card');
+    var cardBodyElement = document.createElement("div");
+    cardBodyElement.classList.add('card-body');
+    $(".card").attr("data-toggle", "modal");
+    $(".card").attr("data-target", "#postModal");
     var postElement = document.createElement("p");
     var placesDetails = document.createElement("p");
     placesDetails.setAttribute("id", "placesDetailsElement");
-        
     postElement.innerHTML = timeConverter(post.when) + " at " + post.where;
-    
     postElement.appendChild(placesDetails);
-    postsLists.appendChild(postElement);
-        
-    //MAPPING
-    var map;
-    var service;
-    var infowindow;
-    initMap();
-    function initMap() {
-      var request = {
-        query: 'Wild Goose Meeting House',
-        fields: ['photos', 'formatted_address', 'name', 'rating', 'opening_hours', 'geometry'],
-      };
+    cardBodyElement.appendChild(postElement);
+    cardElement.appendChild(cardBodyElement);
+    cardElement.setAttribute("id", post.id);
+    cardElement.setAttribute("onclick", "showDetails(this.id)");
+    postsLists.appendChild(cardElement);
 
-      service = new google.maps.places.PlacesService($('#placesDetailsElement').get(0));
-      service.findPlaceFromQuery(request, callback);
-    }
-
-    function callback(results, status) {
-      if (status == google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-          var place = results[i];
-          console.log(place);
-        }
-      }
-    }
-        
+    // Hide the loading animation
     $( "#loading" ).hide();
   })
 })
 
-    
+// Submit entered information to the database
+
 function handleSubmit(e) {
   if (e.type != "click") {
     console.log("it is not a click i guess");
@@ -154,7 +194,7 @@ function handleSubmit(e) {
     return;
   }
 var user = firebase.auth().currentUser;
-    
+
   db.collection("posts").add({
     author: user.uid,
     when: postWhen,
@@ -166,26 +206,34 @@ var user = firebase.auth().currentUser;
     console.log("ERROR: " + error);
   })
 }
-    
+
 }
 }
-/* stupid loading function
-function checkLoad () {
-// jQuery methods go here...
-    var $amountPosts = $("#postsLists > div").length;
-    
-    if ($amountPosts > 1)
-        {
-            $( "#loading" ).hide();
-            $( "#postsLists" ).show( "fast" );
-        }
-    else 
-        {
-            $( "#loading" ).show( "fast" );
-            $( "#postsLists" ).hide();
-        }
+
+
+function showDetails (meetupID) {
+    //document.getElementById('detailPostElement').style.display = none;
+    console.log(meetupID);
+
+    var detailPost = db.collection("posts").doc(meetupID);
+
+    detailPost.get().then(function(doc) {
+    if (doc.exists) {
+        console.log("Document data:", doc.data());
+        document.getElementById('detailHeader').innerHTML = doc.data().where;
+
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+
 }
-*/
+
+// Function to display the UNIX timestamp in a human-readable format (Month Day, Year 00:00)
+
 function timeConverter(UNIX_timestamp){
     var a = new Date(0);
     a.setUTCSeconds(UNIX_timestamp.seconds);
@@ -195,39 +243,8 @@ function timeConverter(UNIX_timestamp){
     var date = (a.getDate() < 10 ? '0' : '') + a.getDate();
     var hour = a.getHours();
     var min = (a.getMinutes() < 10 ? '0' : '') + a.getMinutes();
-    
+
     var time =  month + ' ' + date + ', ' + year + '  ' + hour + ':' + min;
-    
-    /*if (a.year == today.year) {
-        var dateDiff = today.getTime() - a.getTime();
-        var resultInMinutes = Math.round(dateDiff / 60000);
-        var resultInHours = Math.round(dateDiff / 3600000);
-        if (resultInHours > 24)
-            {
-                time =  month + ' ' + date;
-            }
-        else if (resultInHours > 1)
-            {
-                time = resultInHours + " hrs";
-            }
-        else if (resultInMinutes > 59)
-            {
-                time = resultInHours + " hr";
-            }
-        else if (resultInMinutes > 0)
-            {
-                time = resultInMinutes + "m";
-            }
-        else
-            {
-                time = "just now";
-            }
-    }
-    else 
-    {
-        time =  month + ' ' + date + ' ' + year;
-    }*/
 
   return time;
 }
-
